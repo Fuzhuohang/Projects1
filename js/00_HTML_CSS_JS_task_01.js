fda27e9a5a19de315c8dd7b0ccdde0a2cb94608b
Fuzhuohang <995019813@qq.com>
2021-08-27 01:02:12 +0800

    输入类表单元素样式

--- js/00_HTML_CSS_JS_task_01.js
+++ js/00_HTML_CSS_JS_task_01.js
@@ -1,3 +1,53 @@
+var item = document.createElement("div");
+
+var item_name = document.createElement("div");
+
+var item_body = document.createElement("input");
+
+
+window.onload = function() {
+    var header = $("#header").height() + 25;
+    var article = $("#article").width();
+    var form_items = $("#form_items").width();
+    var item_body = $(".item_body").width();
+
+    $("#article").css('top', header);
+    if (article > 700) {
+        $("#form_items").css('width', 200);
+        $("#form_body").css('left', 200);
+    } else if (article > 600) {
+        form_items = article - 500;
+        $("#form_items").css('width', form_items);
+        $("#form_body").css('left', form_items);
+    } else {
+        $("#form_items").css('width', 100);
+        $("#form_body").css('left', 100);
+    }
+    $('.item_value').css('width', item_body);
+
+    $(window).resize(function() {
+        header = $("#header").height() + 25;
+        article = $("#article").width();
+        form_items = $("#form_items").width();
+        item_body = $(".item_body").width();
+
+        $("#article").css('top', header);
+        if (article > 700) {
+            $("#form_items").css('width', 200);
+            $("#form_body").css('left', 200);
+        } else if (article > 600) {
+            form_items = article - 500;
+            $("#form_items").css('width', form_items);
+            $("#form_body").css('left', form_items);
+        } else {
+            $("#form_items").css('width', 100);
+            $("#form_body").css('left', 100);
+        }
+        $('.item_value').css('width', item_body);
+    });
+}
+
+
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -57,5 +107,5 @@ function drop(ev) {
             break;
     }
 
-    ev.target.appendChild(div1);
+    ev.target.appendChild(data);
 }
